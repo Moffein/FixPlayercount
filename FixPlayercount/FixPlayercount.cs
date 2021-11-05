69f44e4864f4df57fdb763328594f1f0d5791374
Moffein <22742925+Moffein@users.noreply.github.com>
2021-11-05 01:46:39 -0700

    Now compatible with Artifact of Multitudes from ZetArtifacts.

--- FixPlayercount/FixPlayercount.cs
+++ FixPlayercount/FixPlayercount.cs
@@ -5,22 +5,31 @@ using R2API.Utils;
 using RoR2;
 using System;
 using System.Runtime.CompilerServices;
+using TPDespair.ZetArtifacts;
 using UnityEngine;
 
 namespace FixPlayercount
 {
     [BepInDependency("com.bepis.r2api")]
     [BepInDependency("dev.wildbook.multitudes", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin("com.Moffein.FixPlayercount", "Fix Playercount", "1.1.0")]
+    [BepInDependency("com.TPDespair.ZetArtifacts", BepInDependency.DependencyFlags.SoftDependency)]
+
+    [BepInPlugin("com.Moffein.FixPlayercount", "Fix Playercount", "1.1.1")]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class FixPlayercount : BaseUnityPlugin
     {
         private static bool MultitudesLoaded = false;
+        private static bool ZetArtifactsLoaded = false;
+
         public void Awake()
         {
             if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("dev.wildbook.multitudes"))
             {
                 MultitudesLoaded = true;
+            }
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.TPDespair.ZetArtifacts"))
+            {
+                ZetArtifactsLoaded = true;
             }
 
             //Based on https://github.com/wildbook/R2Mods/blob/master/Multitudes/Multitudes.cs
@@ -47,6 +56,10 @@ namespace FixPlayercount
             {
                 players = ApplyMultitudes(players);
             }
+            if (ZetArtifactsLoaded)
+            {
+                players = ApplyZetMultitudesArtifact(players);
+            }
             return players;
         }
 
@@ -54,6 +67,19 @@ namespace FixPlayercount
         private static int ApplyMultitudes(int origPlayerCount)
         {
             return origPlayerCount * Multitudes.Multitudes.Multiplier;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static int ApplyZetMultitudesArtifact(int origPlayerCount)
+        {
+            if (RunArtifactManager.instance.IsArtifactEnabled(ZetArtifactsContent.Artifacts.ZetMultifact))
+            {
+                return origPlayerCount * Math.Max(2, ZetArtifactsPlugin.MultifactMultiplier.Value); //GetMultiplier is private so I copypasted the code.
+            }
+            else
+            {
+                return origPlayerCount;
+            }
         }
     }
 }
